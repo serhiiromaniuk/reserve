69188e27be6e68d48fc227a9e1861a2a3baa0297
Serhii Romaniuk <serhii.romaniuk@ambisafe.com>
2022-02-13 16:25:27 +0200

    upd @reserve/database

--- packages/@reserve/database/src/schema/ops/Find.js
+++ packages/@reserve/database/src/schema/ops/Find.js
@@ -2,11 +2,26 @@ import knex from 'knex'
 import { KnexInit } from '../../utils'
 import knexfile from '../../utils/knexfile'
 
-export async function FindData(table, data = '*', lim = 3) {
+export async function FindData(table, data = '*', lim = 3, whereClause = {}, joinParams = null) {
     const tx = KnexInit(knexfile.development)
     try {
-        const query = await tx.select(data).from(table).limit(lim).timeout(4000, { cancel: true })
-        await tx.destroy()
+		let query
+		if (joinParams) {
+			query = await tx
+				.select(data)
+				.from(table)
+				.leftJoin(joinParams[0])
+				.leftJoin(joinParams[1])
+				.where(whereClause).timeout(4000, { cancel: true })
+				.limit(lim)
+		} else {
+			query = await tx
+				.select(data)
+				.from(table)
+				.where(whereClause).timeout(4000, { cancel: true })
+				.limit(lim)
+		}
+		await tx.destroy()
         return {
             ok: true,
             message: query
